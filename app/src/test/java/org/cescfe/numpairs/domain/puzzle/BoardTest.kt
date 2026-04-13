e65d921a21173e0c95e8b11bdcb6fc4efcccf658
FrancescFe <fraferru@gmail.com>
2026-04-13 20:06:49 +0200

    feat(003): implement domain tests

--- app/src/test/java/org/cescfe/numpairs/domain/puzzle/BoardTest.kt
+++ app/src/test/java/org/cescfe/numpairs/domain/puzzle/BoardTest.kt
@@ -5,9 +5,30 @@ import org.junit.Test
 
 class BoardTest {
     @Test
-    fun requires_two_rows_and_four_columns() {
+    fun requires_two_rows() {
         assertThrows(IllegalArgumentException::class.java) {
             Board(tileRows = emptyList())
         }
     }
+
+    @Test
+    fun requires_four_columns_per_row() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Board(
+                tileRows = listOf(
+                    listOf(validTile(1), validTile(2), validTile(3)),
+                    listOf(validTile(4), validTile(5), validTile(6), validTile(7))
+                )
+            )
+        }
+    }
+
+    private fun validTile(leftOperand: Int): Tile = Tile(
+        expression = Expression(
+            leftOperand = leftOperand,
+            operator = Operator.ADDITION,
+            rightOperand = 1
+        ),
+        result = leftOperand + 1
+    )
 }
