01a77f154d6c78ffb5ac4776c56cb585d98f4355
FrancescFe <fraferru@gmail.com>
2026-04-14 21:17:45 +0200

    feat(005): remove strip title

--- app/src/main/java/org/cescfe/numpairs/ui/screen/GameScreen.kt
+++ app/src/main/java/org/cescfe/numpairs/ui/screen/GameScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +51,6 @@ fun GameScreen(puzzle: Puzzle, modifier: Modifier = Modifier.Companion) {
                 .padding(horizontal = 16.dp, vertical = 20.dp),
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            SectionTitle(title = "Strip")
             StripSection(
                 availableNumbers = puzzle.strip.numbers,
                 modifier = Modifier.Companion.fillMaxWidth()
@@ -99,7 +100,9 @@ private fun BoardSection(puzzle: Puzzle, modifier: Modifier = Modifier.Companion
 @Composable
 private fun StripSection(availableNumbers: List<Int>, modifier: Modifier = Modifier.Companion) {
     Surface(
-        modifier = modifier,
+        modifier = modifier.semantics {
+            contentDescription = "Strip"
+        },
         color = MaterialTheme.colorScheme.surfaceVariant,
         shape = RoundedCornerShape(24.dp)
     ) {
