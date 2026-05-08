d0226fa7c059894c52dc183283f43c1d8947fe35
Fran <76252400+FrancescFe@users.noreply.github.com>
2026-05-08 17:26:38 +0200

    [052] Add branded splash screen to the app launch flow (#112)
    * feat(052): implement splash screen
    
    * feat(052): use native splash
    
    * refactor(052): apply spotless

--- app/src/main/java/org/cescfe/numpairs/MainActivity.kt
+++ app/src/main/java/org/cescfe/numpairs/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.cescfe.numpairs.ui.navigation.AppNavigation
 import org.cescfe.numpairs.ui.theme.NumPairsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
