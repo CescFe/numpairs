723d0fc7ae5c2f84d1f957758967c30f7897237e
FrancescFe <fraferru@gmail.com>
2026-05-14 22:58:32 +0200

    feat(078): appNavigation support menu and game navigation

---
+++ app/src/androidTest/java/org/cescfe/numpairs/ui/navigation/AppNavigationTest.kt
@@ -0,0 +1,58 @@
+package org.cescfe.numpairs.ui.navigation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.cescfe.numpairs.feature.game.ui.GameScreenTestTags
+import org.cescfe.numpairs.feature.menu.ui.MenuScreenTestTags
+import org.cescfe.numpairs.ui.theme.NumPairsTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun menuStartDestinationNavigatesToGameWhenTutorialIsSelected() {
+        composeTestRule.setContent {
+            NumPairsTheme {
+                AppNavigation(startDestination = AppDestination.Menu)
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(MenuScreenTestTags.SCREEN)
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithTag(MenuScreenTestTags.TUTORIAL_BUTTON)
+            .performClick()
+
+        composeTestRule
+            .onNodeWithTag(GameScreenTestTags.SCREEN)
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithTag(MenuScreenTestTags.SCREEN)
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun defaultStartDestinationRemainsGame() {
+        composeTestRule.setContent {
+            NumPairsTheme {
+                AppNavigation()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithTag(GameScreenTestTags.SCREEN)
+            .assertIsDisplayed()
+    }
+}
