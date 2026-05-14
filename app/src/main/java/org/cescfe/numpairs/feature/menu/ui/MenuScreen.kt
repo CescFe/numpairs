a18f7039f35d1a00d59195e3f4f4ff0e1a498c9e
FrancescFe <fraferru@gmail.com>
2026-05-14 23:57:28 +0200

    feat(079): remove strings from Menu

--- app/src/main/java/org/cescfe/numpairs/feature/menu/ui/MenuScreen.kt
+++ app/src/main/java/org/cescfe/numpairs/feature/menu/ui/MenuScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.cescfe.numpairs.R
@@ -45,16 +43,6 @@ fun MenuScreen(modifier: Modifier = Modifier, onTutorialSelected: () -> Unit = {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Text(
-                    text = stringResource(R.string.menu_title),
-                    style = MaterialTheme.typography.headlineSmall,
-                    textAlign = TextAlign.Center
-                )
-                Text(
-                    text = stringResource(R.string.menu_supporting_text),
-                    style = MaterialTheme.typography.bodyLarge,
-                    textAlign = TextAlign.Center
-                )
                 Button(
                     onClick = onTutorialSelected,
                     modifier = Modifier.testTag(MenuScreenTestTags.TUTORIAL_BUTTON)
