bac960a0144ee775ce639e2fc2c783728c2761ea
Fran <76252400+FrancescFe@users.noreply.github.com>
2026-05-14 23:02:45 +0200

    [078] Extend AppNavigation to support Menu and Tutorial-to-Game navigation (#170)

--- app/src/main/java/org/cescfe/numpairs/ui/navigation/AppNavigation.kt
+++ app/src/main/java/org/cescfe/numpairs/ui/navigation/AppNavigation.kt
@@ -1,16 +1,32 @@
 package org.cescfe.numpairs.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import org.cescfe.numpairs.feature.game.GameRoute
+import org.cescfe.numpairs.feature.menu.MenuRoute
 
 sealed interface AppDestination {
+    data object Menu : AppDestination
     data object Game : AppDestination
 }
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier, startDestination: AppDestination = AppDestination.Game) {
-    when (startDestination) {
+    var currentDestination by remember(startDestination) {
+        mutableStateOf(startDestination)
+    }
+
+    when (currentDestination) {
+        AppDestination.Menu -> MenuRoute(
+            modifier = modifier,
+            onTutorialSelected = {
+                currentDestination = AppDestination.Game
+            }
+        )
         AppDestination.Game -> GameRoute(modifier = modifier)
     }
 }
