88dc1f729dc055635b19f2e66ae2db775e773906
FrancescFe <fraferru@gmail.com>
2026-06-16 20:14:55 +0200

    feat(128): implement numpairs theme

--- app/src/main/java/org/cescfe/numpairs/ui/theme/Color.kt
+++ app/src/main/java/org/cescfe/numpairs/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package org.cescfe.numpairs.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NumPairsBackground = Color(0xFF0F1512)
+val NumPairsSurface = Color(0xFF171F1A)
+val NumPairsSurfaceRaised = Color(0xFF1D2822)
+val NumPairsSurfaceSubtle = Color(0xFF14201A)
+val NumPairsOnSurface = Color(0xFFF2F0E8)
+val NumPairsOnSurfaceVariant = Color(0xFFB7C0B8)
+val NumPairsOutline = Color(0xFF3A463F)
+val NumPairsOutlineVariant = Color(0xFF27332C)
+val NumPairsJade = Color(0xFF48D597)
+val NumPairsOnJade = Color(0xFF052015)
+val NumPairsJadeSoft = Color(0xFF173A2D)
+val NumPairsOnJadeSoft = Color(0xFFC9F8E2)
+val NumPairsError = Color(0xFFFF8A80)
+val NumPairsErrorSoft = Color(0xFF3B1F1F)
+val NumPairsOnErrorSoft = Color(0xFFFFDAD6)
