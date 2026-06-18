af202d3d8b419229ef55c2aa9ca5745e7b89b8b3
FrancescFe <fraferru@gmail.com>
2026-06-18 19:48:05 +0200

    feat(131): center title and set color as primary

--- app/src/main/java/org/cescfe/numpairs/feature/menu/ui/MenuScreen.kt
+++ app/src/main/java/org/cescfe/numpairs/feature/menu/ui/MenuScreen.kt
@@ -10,10 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -94,9 +95,12 @@ fun MenuScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MenuScreenTopBar() {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
-            Text(text = stringResource(R.string.app_name))
+            Text(
+                text = stringResource(R.string.app_name),
+                color = MaterialTheme.colorScheme.primary
+            )
         }
     )
 }
